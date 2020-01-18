d42c3bfc396594abd0fa595430b26d1930cd26c3
less1226 <jgysmily@msn.com>
2020-01-18 11:24:06 +0800

    add in memory data api, add update hero function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { of, Observable } from 'rxjs';
 import { MessageService } from './message.service';
+
+
+const httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+
+  private log(messge: string) {
+    this.messageService.add(`HeroService: ${messge}`);
+  }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(tap(_ => this.log('fetched heroes')),
+    catchError(this.handleError<Hero[]>('getHeroes', [])));
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id = ${id}`)),
+      catchError(this.handleError<Hero>(`get hero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updated hero'))
+    );
+  }
+
+  private handleError<T>(operation= 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
